dd33003180e29d226943d84779c1bb5d3d400859
Mujtaba12-al <mujtaba@mativeinc.com>
2025-07-28 21:47:19 +0500

    navbar

--- components/home/TabletSection.js
+++ components/home/TabletSection.js
@@ -2,37 +2,10 @@
 
 import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
 
 export default function TabletSection() {
 
-  const textRef = useRef(null);
-  const iframeRef = useRef(null);
-  const mobileTextRef = useRef(null);
-  
-
-  
-
  
-
-  const handleVideoLoad = () => {
-    setIsVideoLoaded(true);
-    setTimeout(() => {
-      setIsHeroLoaded(true);
-    }, 1500);
-  }
-
-  useEffect(() => {
-    const video = document.querySelector('video');
-    if (video) {
-      video.addEventListener('loadeddata', handleVideoLoad);
-      return () => {
-        video.removeEventListener('loadeddata', handleVideoLoad);
-      };
-    }
-  }, []);
 
   return (
     <div
@@ -52,9 +25,9 @@ export default function TabletSection() {
 
       {/* Mobile Screens */}
       <div 
-        className="md:hidden min-h-[357vh] h-screen bg-white flex flex-col justify-center items-center"
+        className="md:hidden xs:min-h-[307vh] min-h-[400vh] h-screen bg-white flex flex-col justify-center items-center"
       >
-        <div className="flex mt-[-6vh] justify-center mobile-text">
+        <div className="flex mt-[8vh] justify-center mobile-text">
           <div className="transform rotate-90 origin-center">
             <h1 className="text-[100vw] font-bold text-black whitespace-nowrap">
               Infocus Media <span className="text-[50vw] align-super">®</span>
@@ -62,6 +35,9 @@ export default function TabletSection() {
           </div>
         </div>
       </div>
+
+
+      
 
       
     </div>
